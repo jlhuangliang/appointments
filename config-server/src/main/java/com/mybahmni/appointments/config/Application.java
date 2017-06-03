b5c6e37c5f4c63e5e3f0a4f182509120b6c0b221
huangliang6 <huangliang6@lenovo.com>
2017-06-03 23:00:17 +0800

    refectory Application.

--- config-server/src/main/java/com/mybahmni/appointments/config/Application.java
+++ config-server/src/main/java/com/mybahmni/appointments/config/Application.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.mybahmni.appointments.config;
 
 import org.springframework.boot.SpringApplication;
@@ -8,30 +5,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
 
-
-/**
- * The Main Spring Boot Application class.<br>
- * <br>
- * 
- * The {@link EnableConfigServer} annotation defines that this application will
- * serve as the REST based API for providing external configuration. <br>
- * <br>
- * 
- * The external repository from where the configuration will be picked up is
- * defined in the {@linkplain application.yml} file.
- * 
- * @author rohitghatol
- */
 @EnableAutoConfiguration
 @EnableConfigServer
 @ComponentScan
 public class Application {
-	
-	/**
-	 * The main method.
-	 *
-	 * @param args the arguments
-	 */
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
