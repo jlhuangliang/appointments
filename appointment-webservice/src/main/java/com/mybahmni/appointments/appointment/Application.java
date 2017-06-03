af626e7474bb9a0c7115b5abf62fc389211c9b41
huangliang6 <huangliang6@lenovo.com>
2017-06-03 19:33:40 +0800

    using feign client in doctor service.

--- appointment-webservice/src/main/java/com/mybahmni/appointments/appointment/Application.java
+++ appointment-webservice/src/main/java/com/mybahmni/appointments/appointment/Application.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @ComponentScan
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableFeignClients
 public class Application {
     public static void main(String[] args) {
 
--- appointment-webservice/src/main/java/com/mybahmni/appointments/appointment/apis/DoctorsService.java
+++ appointment-webservice/src/main/java/com/mybahmni/appointments/appointment/apis/DoctorsService.java
@@ -3,43 +3,26 @@ package com.mybahmni.appointments.appointment.apis;
 import com.mybahmni.appointments.appointment.model.DoctorResource;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Date;
 
-/**
- * The {@link HystrixCommand} works since Spring makes a proxy to intercept the
- * request and call the fallback method if the method errs.<br>
- * <br>
- * 
- * Hence the {@link HystrixCommand} needs to be in separate {@link Component} or
- * {@link Service} stereotypes so that Spring can proxy them.
- * 
- * @author anilallewar
- *
- */
-@Service
-public class DoctorsService {
+@FeignClient("doctor-webservice")
+public interface DoctorsService {
 
-	@Autowired
-	private RestTemplate restTemplate;
+    @HystrixCommand(fallbackMethod = "getFallbackCommentsForTask", commandProperties = {
+        @HystrixProperty(name = "execution.isolation.strategy", value = "SEMAPHORE"), @HystrixProperty(name =
+        "circuitBreaker.requestVolumeThreshold", value = "10"), @HystrixProperty(name = "circuitBreaker" +
+		".sleepWindowInMilliseconds", value = "1000")})
+    @RequestMapping("/doctors/{doctorId}")
+    DoctorResource getDoctorForAppointment(@PathVariable("doctorId") String doctorId);
 
-	@HystrixCommand(fallbackMethod = "getFallbackCommentsForTask", commandProperties = {
-			@HystrixProperty(name = "execution.isolation.strategy", value = "SEMAPHORE"),
-			@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
-			@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "1000") })
-	public DoctorResource getDoctorForAppointment(String doctorId) {
-		String url = String.format("http://doctor-webservice/doctors/%s", doctorId);
-		return restTemplate.getForObject(url, DoctorResource.class);
+    static DoctorResource getFallbackCommentsForTask(String taskId) {
 
-	}
-
-	@SuppressWarnings("unused")
-	private DoctorResource getFallbackCommentsForTask(String taskId) {
-
-		return new DoctorResource("default", "default doctor", new Date());
-	}
+        return new DoctorResource("default", "default doctor", new Date());
+    }
 }
