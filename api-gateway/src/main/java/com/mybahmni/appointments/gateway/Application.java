e4a424d20eb28b3ac7d9bad46578f0c70c91d9a2
huangliang6 <huangliang6@lenovo.com>
2017-06-03 20:15:53 +0800

    refectory.

--- api-gateway/src/main/java/com/mybahmni/appointments/gateway/Application.java
+++ api-gateway/src/main/java/com/mybahmni/appointments/gateway/Application.java
@@ -18,11 +18,6 @@ import org.springframework.context.annotation.Configuration;
 @EnableEurekaClient
 public class Application {
 
-    /**
-     * The main method.
-     *
-     * @param args the arguments
-     */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
